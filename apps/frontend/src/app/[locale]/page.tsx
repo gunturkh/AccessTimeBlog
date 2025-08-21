d3b5c5ce505858a1e7f5b10fe59bc9062024ca0a
Guntur KH <guntur.kh@gmail.com>
2025-08-22 02:27:12 +0700

    feat: localization

--- apps/frontend/src/app/[locale]/page.tsx
+++ apps/frontend/src/app/[locale]/page.tsx
@@ -4,8 +4,10 @@
 import { useGetBlogs } from "@/hooks/useGetBlogs";
 import Link from "next/link";
 import Image from 'next/image'
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations();
   const { data: blogsList, isLoading: isBlogsLoading } = useGetBlogs();
   return (
 
@@ -16,9 +18,9 @@ export default function Home() {
             <h1>
               <span className="block font-display text-base font-semibold text-dark">Blog</span>
               <span className="sr-only"> - </span>
-              <span className="mt-6 block max-w-5xl font-display text-5xl font-bold tracking-tight text-dark [text-wrap:balance] sm:text-6xl">The latest articles and news</span>
+              <span className="mt-6 block max-w-5xl font-display text-5xl font-bold tracking-tight text-dark [text-wrap:balance] sm:text-6xl">{t("Blog.title")}</span>
             </h1>
-            <div className="mt-6 max-w-4xl text-xl text-dark"><p>Dive into our world of web accessibility and software development. Our team of passionate experts shares insights, tips, and best practices to help you become a better developer and create more inclusive digital experiences.</p>
+            <div className="mt-6 max-w-4xl text-xl text-dark"><p>{t("Blog.description")}</p>
             </div>
           </div>
         </div>
@@ -43,7 +45,7 @@ export default function Home() {
                         <dl className="lg:absolute lg:left-0 lg:top-0 lg:w-1/3 lg:px-4">
                           <dt className="sr-only">Published</dt>
                           <dd className="absolute left-0 top-0 text-sm text-dark lg:static">
-                            <time dateTime={blog.publishedAt}>{blog.publishedAt}</time>
+                            <time dateTime={blog.publishedAt}>{new Date(blog.publishedAt).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })}</time>
                           </dd><dt className="sr-only">Author</dt>
                           <dd className="mt-6 flex gap-x-4">
                             <div className="flex-none overflow-hidden rounded-xl bg-neutral-100">
@@ -58,8 +60,8 @@ export default function Home() {
                           </dd>
                         </dl>
                         <p className="mt-6 max-w-2xl text-base text-dark">{blog.description}</p>
-                        <a className="mt-8 inline-flex rounded-full px-4 py-1.5 text-sm font-semibold transition bg-dark text-white hover:bg-dark/90" aria-label={`${blog.title}`} href={`/blog/${blog.slug}`}>
-                          <span className="relative top-px">Read more</span>
+                        <a className="mt-8 inline-flex rounded-full px-4 py-2 text-md font-extrabold transition bg-dark text-white hover:bg-dark/90" aria-label={`${blog.title}`} href={`/blog/${blog.slug}`}>
+                          <span className="relative top-px">{t("Blog.readMore")}</span>
                         </a>
                       </div>
                     </div>
